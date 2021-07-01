35926414b8e07fe7314450dbb322d94ab6c6ff13
Roy Thia <roy.thia@oss.com.ph>
2021-07-01 19:22:15 +0800

    Added 52.149.194.33

--- iptables.sh
+++ iptables.sh
@@ -145,6 +145,7 @@ iptables -I INPUT -s 136.158.33.202 -j DROP
 iptables -I INPUT -s 136.158.32.22 -j DROP
 iptables -I INPUT -s 136.158.33.233 -j DROP
 iptables -I INPUT -s 136.158.32.72 -j DROP
+iptables -I INPUT -s 52.149.194.33 -j DROP
 
 #test git push
 
